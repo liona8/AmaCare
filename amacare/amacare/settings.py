b047d935c6103fe26eed773865783c17c37a6b87
Tok Pei Ying <115140855+liona8@users.noreply.github.com>
2026-05-10 22:23:06 +0800

    Fix Django settings for Vercel production deployment

--- amacare/amacare/settings.py
+++ amacare/amacare/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ylw!z-25rt-ai)2g*4noi6sn^5&03e*!@pa+!#!+kn-2s5w41r'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ylw!z-25rt-ai)2g*4noi6sn^5&03e*!@pa+!#!+kn-2s5w41r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
